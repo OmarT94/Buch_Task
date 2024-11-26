6c50ea90da9ce797b9170be2fd686ff2b049c5bb
OmarT94 <93581484+OmarT94@users.noreply.github.com>
2024-11-26 16:17:03 +0100

    Implementierung_Von_ADD_DELETE_Methoden

--- src/main/java/org/example/Bibliothek.java
+++ src/main/java/org/example/Bibliothek.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Bibliothek {
@@ -17,6 +18,20 @@ private Buch[] buecher;
 
     public Bibliothek(Buch[] buecher) {
     this.buecher =buecher;
+
+}
+
+
+public void addBuch(Buch buch) {
+        Buch[] neueBuch=Arrays.copyOf(buecher,buecher.length + 1);
+        neueBuch[neueBuch.length-1]=buch;
+        buecher=neueBuch;
+}
+
+public void deleteBuch(Buch buch) {
+    ArrayList<Buch> buecherListe=new ArrayList<>(Arrays.asList(buecher));
+    buecherListe.remove(buch);
+    this.buecher=buecherListe.toArray(new Buch[buecherListe.size()]);
 
 }
 
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,12 +3,22 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        Buch buch = new Buch("testTitel","testAutor","testISBN");
-        Buch buch1 = new Buch("HamburfTest!","autorOmar","AbS123");
+        Buch buch  =new Buch("testTitel","testAutor","testISBN");
+        Buch buch1 =new Buch("HamburgTest!","autorOmar","AbS123");
+        Buch buch3 =new Buch("TestAdd_Delete_Methode","TestAdd_Delete_Methode","TestAdd_Delete_Methode");
 
 
         Bibliothek bibliothek= new Bibliothek( new Buch[]{buch,buch1});
 
+        System.out.println("Bibliothek bevor add oder delete Methode ");
+        System.out.println(bibliothek);
+
+        bibliothek.addBuch(buch3);
+        System.out.println("Add_Methode");
+        System.out.println(bibliothek);
+
+        bibliothek.deleteBuch(buch3);
+        System.out.println("Delete_Methode");
         System.out.println(bibliothek);
     }
 }
